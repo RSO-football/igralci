bef5d3b70e1e37b99aa262e0983b08d9c1c35139
UniWorksTS <ts4894@student.uni-lj.si>
2021-11-23 14:26:42 +0100

    check CI/CD

--- api/src/main/java/rso/football/igralci/api/v1/IgralciMetadataApplication.java
+++ api/src/main/java/rso/football/igralci/api/v1/IgralciMetadataApplication.java
@@ -3,6 +3,8 @@ package rso.football.igralci.api.v1;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+//some change 
+
 @ApplicationPath("/v1")
 public class IgralciMetadataApplication extends Application {
 
