5f1f4bf9454472cc12f6e7c400056c8666fb0500
Rok Bosil <rb2600@student.uni-lj.si>
2022-01-05 17:39:14 +0100

    Added openApi

--- api/src/main/java/rso/football/igralci/api/v1/IgralciMetadataApplication.java
+++ api/src/main/java/rso/football/igralci/api/v1/IgralciMetadataApplication.java
@@ -1,10 +1,21 @@
 package rso.football.igralci.api.v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 //some change
 
+//TODO: change url to not localhost
+@OpenAPIDefinition(info = @Info(title = "Igralci API", version = "v1",
+        contact = @Contact(email = "rb2600@student.uni-lj.si"),
+        license = @License(name = "dev"), description = "API for managing igralci metadata."),
+        servers = @Server(url = "http://localhost:8081/"))
 @ApplicationPath("/v1")
 public class IgralciMetadataApplication extends Application {
 
--- api/src/main/java/rso/football/igralci/api/v1/resources/IgralciMetadataResource.java
+++ api/src/main/java/rso/football/igralci/api/v1/resources/IgralciMetadataResource.java
@@ -1,6 +1,15 @@
 package rso.football.igralci.api.v1.resources;
 
 import com.kumuluz.ee.cors.annotations.CrossOrigin;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.headers.Header;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import rso.football.igralci.lib.IgralciMetadata;
 import rso.football.igralci.services.beans.IgralciMetadataBean;
 
@@ -29,6 +38,13 @@ public class IgralciMetadataResource {
     @Context
     protected UriInfo uriInfo;
 
+    @Operation(description = "Get all igralci metadata.", summary = "Get all metadata")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "List of igralci metadata",
+                    content = @Content(schema = @Schema(implementation = IgralciMetadata.class, type = SchemaType.ARRAY)),
+                    headers = {@Header(name = "X-Total-Count", description = "Number of objects in list")}
+            )})
     @GET
     public Response getImageMetadata() {
 
@@ -37,9 +53,17 @@ public class IgralciMetadataResource {
         return Response.status(Response.Status.OK).entity(igralciMetadata).build();
     }
 
+    @Operation(description = "Get metadata for one igralec.", summary = "Get metadata for one igralec")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Igrisce metadata",
+                    content = @Content(
+                            schema = @Schema(implementation = IgralciMetadata.class))
+            )})
     @GET
     @Path("/{igralciMetadataId}")
-    public Response getImageMetadata(@PathParam("igralciMetadataId") Integer igralciMetadataId) {
+    public Response getImageMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                         @PathParam("igralciMetadataId") Integer igralciMetadataId) {
 
         IgralciMetadata igralciMetadata = igralciMetadataBean.getIgralciMetadata(igralciMetadataId);
 
@@ -50,8 +74,18 @@ public class IgralciMetadataResource {
         return Response.status(Response.Status.OK).entity(igralciMetadata).build();
     }
 
+    @Operation(description = "Add igralec metadata.", summary = "Add metadata")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Metadata successfully added."
+            ),
+            @APIResponse(responseCode = "400", description = "Bad request.")
+    })
     @POST
-    public Response createIgralciMetadata(IgralciMetadata igralciMetadata) {
+    public Response createIgralciMetadata(@RequestBody(
+            description = "DTO object with igralci metadata.",
+            required = true, content = @Content(
+            schema = @Schema(implementation = IgralciMetadata.class))) IgralciMetadata igralciMetadata) {
 
         if ((igralciMetadata.getName() == null || igralciMetadata.getDescription() == null)) {
             return Response.status(Response.Status.BAD_REQUEST).build();
@@ -60,14 +94,27 @@ public class IgralciMetadataResource {
             igralciMetadata = igralciMetadataBean.createIgralciMetadata(igralciMetadata);
         }
 
-        return Response.status(Response.Status.CONFLICT).entity(igralciMetadata).build();
+        return Response.status(Response.Status.CREATED).entity(igralciMetadata).build();
 
     }
 
+    @Operation(description = "Update metadata for on igralec.", summary = "Update metadata")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "204",
+                    description = "Metadata successfully updated."
+            ),
+            @APIResponse(responseCode = "404", description = "Not found.")
+    })
     @PUT
     @Path("{igralciMetadataId}")
-    public Response putImageMetadata(@PathParam("igralciMetadataId") Integer igralciMetadataId,
-                                     IgralciMetadata igralciMetadata) {
+    public Response putImageMetadata(@Parameter(description = "Metadata ID.", required = true) 
+                                     @PathParam("igralciMetadataId") Integer igralciMetadataId,
+                                     @RequestBody(
+                                             description = "DTO object with igralec metadata.",
+                                             required = true, content = @Content(
+                                             schema = @Schema(implementation = IgralciMetadata.class)))
+                                             IgralciMetadata igralciMetadata) {
 
         igralciMetadata = igralciMetadataBean.putIgralciMetadata(igralciMetadataId, igralciMetadata);
 
@@ -75,13 +122,25 @@ public class IgralciMetadataResource {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        return Response.status(Response.Status.NOT_MODIFIED).build();
+        return Response.status(Response.Status.NO_CONTENT).build();
 
     }
 
+    @Operation(description = "Delete metadata for one igralec.", summary = "Delete metadata")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "204",
+                    description = "Metadata successfully deleted."
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Not found."
+            )
+    })
     @DELETE
     @Path("{igralciMetadataId}")
-    public Response deleteIgralciMetadata(@PathParam("igralciMetadataId") Integer igralciMetadataId) {
+    public Response deleteIgralciMetadata(@Parameter(description = "Metadata ID.", required = true)
+                                              @PathParam("igralciMetadataId") Integer igralciMetadataId) {
 
         boolean deleted = igralciMetadataBean.deleteIgralciMetadata(igralciMetadataId);
 
